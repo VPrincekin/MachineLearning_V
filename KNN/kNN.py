1dd01bf5b2fcc4d580fb0994dde1d6668116228c
VPrincekin <dehong_w@163.com>
2017-12-15 19:17:11 +0800

    Update代码至python3.6

--- KNN/kNN.py
+++ KNN/kNN.py
@@ -106,9 +106,9 @@ def datingClassTest():
 #约会网站预测函数
 def classifyPerson():
     resultList = ['not at all','in small doses','in large doses']
-    percentTats = float(raw_input("percentage of time spent playing video games?"))
-    ffMiles = float(raw_input("frequent flier miles earned per year?"))
-    iceCream = float(raw_input("liters of ice cream consumed per year?"))
+    percentTats = float(input("percentage of time spent playing video games?"))
+    ffMiles = float(input("frequent flier miles earned per year?"))
+    iceCream = float(input("liters of ice cream consumed per year?"))
     datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
     normMat,ranges,minVals = autoNorm(datingDataMat)
     inArr = array([ffMiles,percentTats,iceCream])
